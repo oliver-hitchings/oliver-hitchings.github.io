31111fa5518a19743418d8a6738c21c2fb219b54
Oliver Hitchings <35489433+oliver-hitchings@users.noreply.github.com>
2018-05-25 14:21:25 +0100

    Update login.js

--- login.js
+++ login.js
@@ -4,9 +4,9 @@
   const buttonLogin = document.getElementById("buttonLogin");
 
   buttonLogin.addEventListener('click', e => {
-    if ((txtUsername.value.toLowerCase() == "oliver") && (txtPassword.value == "fish")) {
+    if ((txtUsername.value.toLowerCase() == "admin") && (txtPassword.value == "password")) {
       window.location.replace("/login/oli.html")
-    } else if ((txtUsername.value.toLowerCase() == "admin") && (txtPassword.value == "pasword")) {
+    } else if ((txtUsername.value.toLowerCase() == "admin") && (txtPassword.value == "password")) {
       window.location.replace("/login/daniel.html")
     } else {
       alert("Invalid Credentials")
